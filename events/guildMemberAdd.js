7065d03347d5e34ffc6a34ed8a21016adaacc97a
Kevin D <kevin.diangelis@gmail.com>
2022-07-27 22:42:27 -0400

    Add embeds because I hate life and embeds!

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -2,6 +2,15 @@ module.exports = {
     name: 'guildMemberAdd',
     once: false,
     execute(member) {
-        member.guild.channels.cache.find(c => c.name == "chatting").send(`${member.user.username} has joined RipMC.`)
+        const joinEmbed = new EmbedBuilder()
+        .setColor(embedColor)
+        .addFields(
+            {name: "\u200B", value: `**New member!**
+            
+            Welcome **${member.user.username}** to RipMC!
+            Please read <#793249977916194846> & <#792947057517133905> for more information!
+            `}
+        )
+        member.guild.channels.cache.find(c => c.name == "chatting").send({embeds: [joinEmbed]});
     }
 }
